d58f3a4940a0c9eafbaabb3917f450219f71abcf
Dima Potekhin <untrots@gmail.com>
2014-12-31 16:46:23 +0200

    +WorkspaceEntity

--- projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/__init__.py
+++ projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/__init__.py
@@ -2,6 +2,7 @@ from DerKonfigurator import DerKonfigurator
 from DirectoryBasedObject import DirectoryBasedObject
 from Environment import Environment
 from Resources import Resources
+from WorkspaceEntity import WorkspaceEntity
 from utils import read_binary_file, write_binary_file, read_unicode_file, write_unicode_file, read_yaml, ensure_dir
 
 __ALL__ = [
@@ -9,6 +10,7 @@ __ALL__ = [
     'DirectoryBasedObject',
     'Environment',
     'Resources',
+    'WorkspaceEntity',
 
     # utils
     'read_binary_file',
