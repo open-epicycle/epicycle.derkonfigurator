b9efa9b26c52e2220d16a6866baff6e1de99135b
Dima Potekhin <untrots@gmail.com>
2014-12-31 15:15:18 +0200

    +Resources

--- projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/__init__.py
+++ projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/__init__.py
@@ -1,8 +1,10 @@
 from DerKonfigurator import DerKonfigurator
+from Resources import Resources
 from utils import read_binary_file, write_binary_file, read_unicode_file, write_unicode_file, read_yaml, ensure_dir
 
 __ALL__ = [
     'DerKonfigurator',
+    'Resources',
 
     # utils
     'read_binary_file',
