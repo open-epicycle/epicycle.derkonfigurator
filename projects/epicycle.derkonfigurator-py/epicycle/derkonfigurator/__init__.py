94bfe69f4280e4c57d6fe47ac19ce545012cca68
Dima Potekhin <untrots@gmail.com>
2014-12-31 16:28:09 +0200

    +DirectoryBasedObject

--- projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/__init__.py
+++ projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/__init__.py
@@ -1,9 +1,11 @@
 from DerKonfigurator import DerKonfigurator
+from DirectoryBasedObject import DirectoryBasedObject
 from Resources import Resources
 from utils import read_binary_file, write_binary_file, read_unicode_file, write_unicode_file, read_yaml, ensure_dir
 
 __ALL__ = [
     'DerKonfigurator',
+    'DirectoryBasedObject',
     'Resources',
 
     # utils
