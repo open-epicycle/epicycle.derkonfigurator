3dc833e62159aba11022a5726f63b74c2b5e7689
Dima Potekhin <untrots@gmail.com>
2015-01-05 20:24:45 +0200

    ProjectConfiguratorCs: finding all project's source files

--- projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/project/ProjectConfiguratorCs.py
+++ projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/project/ProjectConfiguratorCs.py
@@ -8,14 +8,25 @@ class ProjectConfiguratorCs(ProjectConfigurator):
     def __init__(self, project):
         super(ProjectConfiguratorCs, self).__init__(project)
 
+        self._source_files = []
+
         self._guid = nget(self.project.config, "guid")
 
     @property
     def guid(self):
         return self._guid
 
+    @property
+    def source_files(self):
+        return self._source_files
+
     def _configure(self):
+        self._find_source_files()
         self._generate_assemblyinfo()
+
+    def _find_source_files(self):
+        self._source_files = self.project.directory.find_files_rec(extension=".cs", ignore_dirs=['obj', 'bin'])
+        self.project.report("Found %d source files" % len(self.source_files))
 
     def _generate_assemblyinfo(self):
         self.project.report("Generating AssemblyInfo")
