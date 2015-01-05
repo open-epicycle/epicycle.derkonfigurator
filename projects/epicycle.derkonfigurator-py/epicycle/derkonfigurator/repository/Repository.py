1fc15825a6245d7ecf104787d34130b304530106
Dima Potekhin <untrots@gmail.com>
2015-01-05 19:21:11 +0200

    Repository: moving the version to a dedicated file

--- projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/repository/Repository.py
+++ projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/repository/Repository.py
@@ -11,6 +11,7 @@ from epicycle.derkonfigurator.project.Project import Project
 
 
 class Repository(WorkspaceEntity):
+    DEFAULT_VERSION = "0.0.0.0"
     CONFIG_FILE_NAME = "repository_config.yaml"
     PROJECTS_DIR = "projects"
 
@@ -20,7 +21,8 @@ class Repository(WorkspaceEntity):
         self._config = self.directory.read_yaml(Repository.CONFIG_FILE_NAME)
         self._name = os.path.split(path)[1]
 
-        self._version = nget(self._config, "version", default="")
+        version_data = self.directory.read_unicode_file("version")
+        self._version = version_data.strip() if version_data.strip() else Repository.DEFAULT_VERSION
         self._organization = nget(self._config, "organization", default="")
         self._product = nget(self._config, "product", default=self.name)
         self._copyright = nget(self._config, "copyright", default="")
