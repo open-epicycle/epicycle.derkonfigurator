23094a5166d595f99257b9f9baa4e470b9e11464
Dima Potekhin <untrots@gmail.com>
2015-01-05 15:36:36 +0200

    Cosmetics

--- projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/project/Project.py
+++ projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/project/Project.py
@@ -1,4 +1,4 @@
-__author__ = 'dima'
+__author__ = 'Dima Potekhin'
 
 import os
 from epicycle.derkonfigurator.WorkspaceEntity import WorkspaceEntity
@@ -10,7 +10,7 @@ class Project(WorkspaceEntity):
     def __init__(self, parent, path):
         super(Project, self).__init__(path, parent.environment, parent.workspace, parent.reporter)
 
-        self._config = self.read_yaml(Project.CONFIG_FILE_NAME)
+        self._config = self.directory.read_yaml(Project.CONFIG_FILE_NAME)
         self._name = os.path.split(path)[1]
 
     @property
@@ -20,4 +20,4 @@ class Project(WorkspaceEntity):
     def configure(self):
         self.report("Configuring the project %s" % self.name)
 
-        # TODO
+        # TODO-
