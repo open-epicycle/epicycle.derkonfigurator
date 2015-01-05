31f19eb674561764de8cd59e65dbcdc05b77f802
Dima Potekhin <untrots@gmail.com>
2015-01-05 14:44:22 +0200

    Repository loads and configures projects

--- projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/repository/Repository.py
+++ projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/repository/Repository.py
@@ -5,18 +5,20 @@ Contains the Repository class
 """
 
 import os
-
 from epicycle.derkonfigurator.WorkspaceEntity import WorkspaceEntity
-
+from epicycle.derkonfigurator.project.Project import Project
 
 class Repository(WorkspaceEntity):
     CONFIG_FILE_NAME = "repository_config.yaml"
+    PROJECTS_DIR = "projects"
 
     def __init__(self, parent, path):
         super(Repository, self).__init__(path, parent.environment, parent.workspace, parent.reporter)
         
         self._config = self.read_yaml("repository_config.yaml")
         self._name = os.path.split(path)[1]
+
+        self._projects = []
 
     @property
     def name(self):
@@ -26,6 +28,14 @@ class Repository(WorkspaceEntity):
         self.report("Configuring the repository %s" % self.name)
 
         with self.report_sub_level():
-            self.report("Finished configuring the repository!")
+            self._load_projects()
+            self._configure_projects()
 
-        return True
+    def _load_projects(self):
+        for item, full_path in self.listdir_dirs_with_file_full(Project.CONFIG_FILE_NAME, self.PROJECTS_DIR):
+            self._projects.append(Project(self.workspace, full_path))
+
+    def _configure_projects(self):
+        with self.report_sub_level():
+            for project in self._projects:
+                project.configure()
