8cd38940b3a5bd818f48f30c3c30aa3a43171e7f
Dima Potekhin <untrots@gmail.com>
2015-01-05 19:27:32 +0200

    Directory: better names for parameters

--- projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/Directory.py
+++ projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/Directory.py
@@ -12,14 +12,14 @@ class Directory(object):
     def path(self):
         return self._path
 
-    def to_full_path(self, *sub_path_parts):
-        return os.path.join(self._path, *sub_path_parts)
+    def to_full_path(self, *subpath_parts):
+        return os.path.join(self._path, *subpath_parts)
 
-    def subdir(self, *sub_path_parts):
-        return Directory(self.to_full_path(*sub_path_parts))
+    def subdir(self, *subpath_parts):
+        return Directory(self.to_full_path(*subpath_parts))
 
-    def ensure_subdir(self, *sub_path_parts):
-        full_path = self.to_full_path(*sub_path_parts)
+    def ensure_subdir(self, *subpath_parts):
+        full_path = self.to_full_path(*subpath_parts)
         ensure_dir(full_path)
 
         return Directory(full_path)
@@ -30,26 +30,26 @@ class Directory(object):
                 in listdir_full(self.path)
                 if os.path.isdir(full_path)]
 
-    def list_subdirs_with_file(self, file_name):
-        return [directory for directory in self.list_subdirs() if directory.contains_file(file_name)]
+    def list_subdirs_with_file(self, file_subpath):
+        return [directory for directory in self.list_subdirs() if directory.contains_file(file_subpath)]
 
-    def contains_file(self, file_name):
-        return os.path.isfile(self.to_full_path(file_name))
+    def contains_file(self, file_subpath):
+        return os.path.isfile(self.to_full_path(file_subpath))
 
-    def contains_subdir(self, file_name):
-        return os.path.isdir(self.to_full_path(file_name))
+    def contains_subdir(self, file_subpath):
+        return os.path.isdir(self.to_full_path(file_subpath))
 
-    def read_binary_file(self, file_name):
-        return read_binary_file(self.to_full_path(file_name))
+    def read_binary_file(self, file_subpath):
+        return read_binary_file(self.to_full_path(file_subpath))
 
-    def write_binary_file(self, file_name, data):
-        return write_binary_file(self.to_full_path(file_name), data)
+    def write_binary_file(self, file_subpath, data):
+        return write_binary_file(self.to_full_path(file_subpath), data)
 
-    def read_unicode_file(self, file_name):
-        return read_unicode_file(self.to_full_path(file_name))
+    def read_unicode_file(self, file_subpath):
+        return read_unicode_file(self.to_full_path(file_subpath))
 
-    def write_unicode_file(self, file_name, data):
-        return write_unicode_file(self.to_full_path(file_name), data)
+    def write_unicode_file(self, file_subpath, data):
+        return write_unicode_file(self.to_full_path(file_subpath), data)
 
-    def read_yaml(self, file_name):
-        return read_yaml(self.to_full_path(file_name))
+    def read_yaml(self, file_subpath):
+        return read_yaml(self.to_full_path(file_subpath))
