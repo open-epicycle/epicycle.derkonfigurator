245dcdf5c90860a3f16e0bf2c3fdc387e5f5fa94
Dima Potekhin <untrots@gmail.com>
2015-01-06 04:12:18 +0200

    ProjectConfiguratorCs: adding the ability to generate infocomments for source files

--- projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/project/ProjectConfiguratorCs.py
+++ projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/project/ProjectConfiguratorCs.py
@@ -5,6 +5,8 @@ from epicycle.derkonfigurator.utils import nget
 
 
 class ProjectConfiguratorCs(ProjectConfigurator):
+    _SOURCE_INFOCOMMENT_INSERTOID_ID = "INFO"
+
     def __init__(self, project):
         super(ProjectConfiguratorCs, self).__init__(project)
 
@@ -23,6 +25,7 @@ class ProjectConfiguratorCs(ProjectConfigurator):
     def _configure(self):
         self._find_source_files()
         self._generate_assemblyinfo()
+        self._generate_source_infocomments()
 
     def _find_source_files(self):
         self._source_files = self.project.directory.find_files_rec(extension=".cs", ignore_dirs=['obj', 'bin'])
@@ -41,3 +44,23 @@ class ProjectConfiguratorCs(ProjectConfigurator):
             product=self.project.repository.product,
             copyright=self.project.repository.copyright,
         )
+
+    def _generate_source_infocomments(self):
+        infocomment = self._generate_infocomment()
+
+        insertoid_name = ProjectConfiguratorCs._SOURCE_INFOCOMMENT_INSERTOID_ID
+
+        for source_file in self._source_files:
+            if not self.project.has_insertoid(source_file, insertoid_name):
+                self.project.report("WARNING: No %s insertoid in %s" % (insertoid_name, source_file))
+
+            self.project.write_insertoid(source_file, insertoid_name, infocomment)
+
+    def _generate_infocomment(self):
+        raw_comment = self.project.repository.source_infocomment
+
+        if not raw_comment:
+            return ""
+
+        lines = raw_comment.split("\n")
+        return "\n%s\n// " % "\n".join(["// " + x for x in lines])
