35de3aea9c3cc6f0f43913d6b90281f0128c828c
Dima Potekhin <untrots@gmail.com>
2015-01-06 15:45:05 +0200

    ProjectConfiguratorCs: Partial generation of vsproj file.

--- projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/project/ProjectConfiguratorCs.py
+++ projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/project/ProjectConfiguratorCs.py
@@ -12,11 +12,16 @@ class ProjectConfiguratorCs(ProjectConfigurator):
 
         self._source_files = []
 
-        self._guid = nget(self.project.config, "guid")
+        self._project_guid = nget(self.project.config, "project_guid")
+        self._assemblyinfo_guid = nget(self.project.config, "assemblyinfo_guid")
 
     @property
-    def guid(self):
-        return self._guid
+    def project_guid(self):
+        return self._project_guid
+
+    @property
+    def assemblyinfo_guid(self):
+        return self._assemblyinfo_guid
 
     @property
     def source_files(self):
@@ -26,6 +31,7 @@ class ProjectConfiguratorCs(ProjectConfigurator):
         self._find_source_files()
         self._generate_assemblyinfo()
         self._generate_source_infocomments()
+        self._generate_vs_project_file()
 
     def _find_source_files(self):
         self._source_files = self.project.directory.find_files_rec(extension=".cs", ignore_dirs=['obj', 'bin'])
@@ -36,7 +42,7 @@ class ProjectConfiguratorCs(ProjectConfigurator):
 
         self.project.write_template(
             "Properties/AssemblyInfo.cs", "templates/cs/AssemblyInfo.TEMPLATE.cs",
-            guid=self.guid,
+            guid=self.assemblyinfo_guid,
             version=self.project.repository.version,
             title=self.project.full_name,
             description=self.project.description,
@@ -46,6 +52,8 @@ class ProjectConfiguratorCs(ProjectConfigurator):
         )
 
     def _generate_source_infocomments(self):
+        self.project.report("Generating source infocomments")
+
         infocomment = self._generate_infocomment()
 
         insertoid_name = ProjectConfiguratorCs._SOURCE_INFOCOMMENT_INSERTOID_ID
@@ -64,3 +72,19 @@ class ProjectConfiguratorCs(ProjectConfigurator):
 
         lines = split_into_lines(raw_comment)
         return "\r\n%s\r\n// " % "\r\n".join(["// " + x for x in lines])
+
+    def _generate_vs_project_file(self):
+        self.project.report("Generating VS proj file")
+
+        proj_file_name = "%s.csproj" % self.project.full_name
+
+        self.project.write_template(
+            proj_file_name, "templates/cs/vs-cs-lib.TEMPLATE.csproj",
+            guid=self.project_guid,
+            assembly_name=self.project.full_name,
+            root_namespace=self.project.name,
+            compile_list=self._generate_csproj_compile_part()
+        )
+
+    def _generate_csproj_compile_part(self):
+        return "\r\n".join(["    <Compile Include=\"%s\" />" % x.replace('/', '\\') for x in self.source_files])
