3d28f0361695b000660c511955746e08e71bfc6a
Dima Potekhin <untrots@gmail.com>
2015-01-07 20:54:08 +0200

    RepositoryConfiguratorCs: The supported .NET frameworks are now configurable

--- projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/repository/RepositoryConfiguratorCs.py
+++ projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/repository/RepositoryConfiguratorCs.py
@@ -1,12 +1,18 @@
 __author__ = 'Dima Potekhin'
 
+from epicycle.derkonfigurator.utils import nget
 from RepositoryConfigurator import RepositoryConfigurator
 
 
 class RepositoryConfiguratorCs(RepositoryConfigurator):
-    def __init__(self, project):
-        super(RepositoryConfiguratorCs, self).__init__(project)
+    def __init__(self, repository):
+        super(RepositoryConfiguratorCs, self).__init__(repository)
+
+        self._supported_frameworks = nget(self.repository.config, "dotnet_frameworks", [])
+
+    @property
+    def supported_frameworks(self):
+        return self._supported_frameworks
 
     def _configure(self):
         self.repository.report("Configuring .NET repository")
-        pass
