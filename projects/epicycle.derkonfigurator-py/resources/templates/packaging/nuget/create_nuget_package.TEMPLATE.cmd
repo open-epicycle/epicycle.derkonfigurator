950f46fa510e1b94d9ba8012daa04f4b8b07b33c
Dima Potekhin <untrots@gmail.com>
2015-01-07 02:14:02 +0200

    create_nuget_package.TEMPLATE.cmd: copying readme and license as well

--- projects/epicycle.derkonfigurator-py/resources/templates/packaging/nuget/create_nuget_package.TEMPLATE.cmd
+++ projects/epicycle.derkonfigurator-py/resources/templates/packaging/nuget/create_nuget_package.TEMPLATE.cmd
@@ -6,6 +6,8 @@ mkdir NuGetPackage\%(package_name)s
 mkdir NuGetPackage\%(package_name)s\lib
 
 copy package.nuspec NuGetPackage\%(package_name)s\%(package_name)s.nuspec
+copy README.md NuGetPackage\%(package_name)s\README.md
+copy LICENSE NuGetPackage\%(package_name)s\LICENSE
 
 %(copy_bin_commands)s
 
