b0abea37fa85218f6a11dead2f9cb9df91fa5cbe
Dima Potekhin <untrots@gmail.com>
2015-01-07 18:28:34 +0200

    +DotNetSystemLib

--- projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/externals/__init__.py
+++ projects/epicycle.derkonfigurator-py/epicycle/derkonfigurator/externals/__init__.py
@@ -1,10 +1,12 @@
 __author__ = 'Dima Potekhin'
 
 from DotNetLib import DotNetLib
+from DotNetSystemLib import DotNetSystemLib
 from ExternalsManager import ExternalsManager
 
 
 __ALL__ = [
     'DotNetLib',
+    'DotNetSystemLib',
     'ExternalsManager',
 ]
